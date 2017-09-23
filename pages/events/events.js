d18e6d759116ec479bb270312389a565bfff9d97
Nathan Lee <kwnath@gmail.com>
2017-09-24 02:48:19 +0800

    added announcements card

--- pages/events/events.js
+++ pages/events/events.js
@@ -9,9 +9,11 @@ Page({
     longitude: ''
   },
   //事件处理函数
-  bindViewTap: function() {
+  viewEvent: function(res) {
+    console.log('Id of event', res.currentTarget.id)
+    const id = res.currentTarget.id
     wx.navigateTo({
-      url: '../logs/logs'
+      url: `../event/event?id=${id}`
     })
   },
   onLoad: function () {
